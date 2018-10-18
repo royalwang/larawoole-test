771ac96085e9e00c73cb91ef48c3dc39ac26ed1c
Lee <235025828@qq.com>
2018-10-18 16:44:11 +0800

    部署sing cut测试服务器

--- config/shouqianba.php
+++ config/shouqianba.php
@@ -1,18 +1,18 @@
 <?php
 
     return [
-        'url' => [
-            'activate' => 'https://api.shouqianba.com/terminal/activate',
-            'checkin' => 'https://api.shouqianba.com/terminal/checkin',
-            'pay' => 'https://api.shouqianba.com/upay/v2/pay',
-            'query' => 'https://api.shouqianba.com/upay/v2/query',
-            'cancel' => 'https://api.shouqianba.com/upay/v2/cancel',
-            'refund' => 'https://api.shouqianba.com/upay/v2/refund',
-            'uploadLog' => 'https://api.shouqianba.com/terminal/upload Log',
-        ],
-//        'device_id' => '11223344', //测试设备ID
-        'device_id' => '9001801000', //正式设备ID
-        //'device_id' => '800700000', //测试设备ID
-        'subject' => 'Sing Cut美发服务',
-        'operator' => 'system',
+        'url'      => [
+            'activate'  => 'https://api.shouqianba.com/terminal/activate' ,
+            'checkin'   => 'https://api.shouqianba.com/terminal/checkin' ,
+            'pay'       => 'https://api.shouqianba.com/upay/v2/pay' ,
+            'query'     => 'https://api.shouqianba.com/upay/v2/query' ,
+            'cancel'    => 'https://api.shouqianba.com/upay/v2/cancel' ,
+            'refund'    => 'https://api.shouqianba.com/upay/v2/refund' ,
+            'uploadLog' => 'https://api.shouqianba.com/terminal/upload Log' ,
+        ] ,
+        // device_id' => '11223344', //测试设备ID
+        //'device_id' => '9001801000', //正式设备ID
+        'device_id' => '800700000', //测试设备ID
+        'subject'  => 'Sing Cut美发服务' ,
+        'operator' => 'system' ,
     ];
