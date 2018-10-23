1d0eea069f1cda1fc32f21d7f4d5565816df2519
Lee <235025828@qq.com>
2018-10-23 14:56:18 +0800

    1、增加创建用户密码必填 2、修复已知bug

--- config/validator.php
+++ config/validator.php
@@ -1,88 +1,88 @@
 <?php
 
-return [
-    'admin' => [
-        'user' => [
-            'update' => [
-                'name' => 'required|max:255',
-                'sex' =>'required',
-                'identity' => 'required',
-//                'email' => 'required|email|max:255|unique:users,email',
-//                'password' => 'nullable|min:6',
-            ],
-            'store' => [
-                'name' => 'required|max:255',
-                'work_id' => 'required|max:128|unique:users,work_id',
-//                'email' => 'required|email|max:255|unique:users',
-                'password' => 'length:6',
-                'sex' =>'required',
-                'identity' => 'required',
-            ],
-        ],
-        'role' => [
-            'update' => [
-                'name' => 'required|max:255|unique:role,name',
-                'display_name' => 'required|max:255',
-            ],
-            'store' => [
-                'name' => 'required|max:255|unique:role,name',
-                'display_name' => 'required|max:255',
-            ],
-        ],
-        'shop' => [
-            'update' => [
-                'name' => 'required|max:255',
-                'address' => 'required|max:255',
-                'discount' => 'required|numeric'
-            ],
-            'store' => [
-                'name' => 'required|max:255',
-                'address' => 'required|max:255',
-                'discount' => 'required|numeric'
-            ],
-        ],
-        'equip' => [
-            'update' => [
-                'name' => 'required|max:255'
-            ],
-            'store' => [
-                'name' => 'required|max:255',
-                'type' => 'required|max:255',
-            ],
-        ],
-        'order' => [
-            'update' => [
-                'name' => 'required|max:255',
-                'type' => 'required|max:255',
-            ],
-            'store' => [
-                'name' => 'required|max:255',
-                'type' => 'required|max:255',
-            ],
-        ],
-        'area' => [
-            'update' => [
-                'name' => 'required|max:255',
-            ],
-            'store' => [
-                'name' => 'required|max:255',
-            ],
-        ],
-        'sp' => [
-            'update' => [
-            ],
-            'store' => [
-            ],
-        ],
-        'teacher' => [
-            'update' =>[
-                'user_id' => 'required | max:5',
-            ]
-        ],
-        'report' => [
-            'store' => [
-                'content' => 'required',
-            ]
-        ]
-    ],
-];
+    return [
+        'admin' => [
+            'user'    => [
+                'update' => [
+                    'name'     => 'required|max:255' ,
+                    'sex'      => 'required' ,
+                    'identity' => 'required' ,
+                    //                'email' => 'required|email|max:255|unique:users,email',
+                    //                'password' => 'nullable|min:6',
+                ] ,
+                'store'  => [
+                    'name'     => 'required|max:255' ,
+                    'work_id'  => 'required|max:128|unique:users,work_id' ,
+                    //                'email' => 'required|email|max:255|unique:users',
+                    'sex'      => 'required' ,
+                    'identity' => 'required' ,
+                    'password' => 'required' ,
+                ] ,
+            ] ,
+            'role'    => [
+                'update' => [
+                    'name'         => 'required|max:255|unique:role,name' ,
+                    'display_name' => 'required|max:255' ,
+                ] ,
+                'store'  => [
+                    'name'         => 'required|max:255|unique:role,name' ,
+                    'display_name' => 'required|max:255' ,
+                ] ,
+            ] ,
+            'shop'    => [
+                'update' => [
+                    'name'     => 'required|max:255' ,
+                    'address'  => 'required|max:255' ,
+                    'discount' => 'required|numeric' ,
+                ] ,
+                'store'  => [
+                    'name'     => 'required|max:255' ,
+                    'address'  => 'required|max:255' ,
+                    'discount' => 'required|numeric' ,
+                ] ,
+            ] ,
+            'equip'   => [
+                'update' => [
+                    'name' => 'required|max:255' ,
+                ] ,
+                'store'  => [
+                    'name' => 'required|max:255' ,
+                    'type' => 'required|max:255' ,
+                ] ,
+            ] ,
+            'order'   => [
+                'update' => [
+                    'name' => 'required|max:255' ,
+                    'type' => 'required|max:255' ,
+                ] ,
+                'store'  => [
+                    'name' => 'required|max:255' ,
+                    'type' => 'required|max:255' ,
+                ] ,
+            ] ,
+            'area'    => [
+                'update' => [
+                    'name' => 'required|max:255' ,
+                ] ,
+                'store'  => [
+                    'name' => 'required|max:255' ,
+                ] ,
+            ] ,
+            'sp'      => [
+                'update' => [
+                ] ,
+                'store'  => [
+                ] ,
+            ] ,
+            'teacher' => [
+                'update' => [
+                    'user_id' => 'required | max:5' ,
+                ] ,
+            ] ,
+            'report'  => [
+                'store' => [
+                    'content' => 'required' ,
+                ] ,
+            ] ,
+        ] ,
+    ];
