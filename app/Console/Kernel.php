1ea4cfd6d745fde36ac92795cc6cda2b4a3d2032
Lee <235025828@qq.com>
2018-10-24 10:23:16 +0800

    增加测试定时任务内容

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -1,45 +1,48 @@
 <?php
 
-namespace App\Console;
+    namespace App\Console;
 
-use App\Console\Commands\SwooleConmmand;
-use Illuminate\Console\Scheduling\Schedule;
-use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-use SwooleTW\Http\Commands\HttpServerCommand;
-use App\Console\Commands\ScehduleTask;
+    use App\Console\Commands\SwooleConmmand;
+    use App\Console\Commands\TestCron;
+    use Illuminate\Console\Scheduling\Schedule;
+    use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+    use SwooleTW\Http\Commands\HttpServerCommand;
+    use App\Console\Commands\ScehduleTask;
 
-class Kernel extends ConsoleKernel
-{
-    /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
-    protected $commands = [
-        SwooleConmmand::class,HttpServerCommand::class,ScehduleTask::class
-    ];
-
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
+    class Kernel extends ConsoleKernel
     {
-         $schedule->command('schedule:start')
-                  ->everyMinute();
-    }
+        /**
+         * The Artisan commands provided by your application.
+         *
+         * @var array
+         */
+        protected $commands = [
+            SwooleConmmand::class , HttpServerCommand::class , ScehduleTask::class , TestCron::class ,
 
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
-    protected function commands()
-    {
-        $this->load(__DIR__.'/Commands');
+        ];
 
-        require base_path('routes/console.php');
+        /**
+         * Define the application's command schedule.
+         *
+         * @param  \Illuminate\Console\Scheduling\Schedule $schedule
+         *
+         * @return void
+         */
+        protected function schedule( Schedule $schedule )
+        {
+            $schedule->command( 'test:cron' )
+                ->everyMinute();
+        }
+
+        /**
+         * Register the commands for the application.
+         *
+         * @return void
+         */
+        protected function commands()
+        {
+            $this->load( __DIR__ . '/Commands' );
+
+            require base_path( 'routes/console.php' );
+        }
     }
-}
--- app/Http/Controllers/Swoole/SwooleController.php
+++ app/Http/Controllers/Swoole/SwooleController.php
@@ -278,6 +278,7 @@
          *
          * @since 1.0
          * @return string
+         * @todo 设置员工机每天特定时间退出登录
          */
         public function proceedData( $data )
         {
