b71da57a4ca2e28c36a49d80d227fbd63f36fdb6
Lee <235025828@qq.com>
2018-11-09 02:10:10 +0800

    修复门店工作情况显示等候客户数错误问题

--- resources/views/vendor/speedy/admin/work/index.blade.php
+++ resources/views/vendor/speedy/admin/work/index.blade.php
@@ -7,49 +7,49 @@
                 <div class="panel panel-info">
                     <div class="panel-heading">{{ trans('view.admin.work.title') }}
                     </div>
-                        <table class="table table-bordered table-hover" style="text-align: center">
-                            <thead>
-                                <tr class="active" style="text-align: center">
-                                    <th style="text-align: center">{{ trans('view.admin.work.shop') }}</th>
-                                    <th style="text-align: center">{{ trans('view.admin.work.manager') }}</th>
-                                    <th style="text-align: center">{{ trans('view.admin.work.staff_num') }}</th>
-                                    <th style="text-align: center">{{ trans('view.admin.work.today_staff') }}
-                                    <th style="text-align: center">{{ trans('view.admin.work.today_order_num') }}
-                                    <th style="text-align: center">{{ trans('view.admin.work.profit') }}
-                                    <th style="text-align: center">{{ trans('view.admin.work.waiting') }}</th>
-                                    <th style="text-align: center">{{ trans('view.admin.work.hint') }}</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach($shops as $shop)
-                                <tr>
-                                    <td>{{ $shop->name }}</td>
-                                    <td>{{ $shop->hasOneManager ? $shop->hasOneManager->display_name:'-' }}</td>
-                                    <td>{{ $shop->hasManyUsers->count() }}</td>
-                                    <td>{{ $shop->onWork }}</td>
-                                    <td>{{ $shop->doneOrders }}</td>
-                                    <td>{{ $shop->todayProfit/100 }}</td>
-                                    <td>{{ $shop->hasManyOrderWaiting->count() }}</td>
-                                    @if($shop->hasManyOrderWaiting->count()<3)
+                    <table class="table table-bordered table-hover" style="text-align: center">
+                        <thead>
+                        <tr class="active" style="text-align: center">
+                            <th style="text-align: center">{{ trans('view.admin.work.shop') }}</th>
+                            <th style="text-align: center">{{ trans('view.admin.work.manager') }}</th>
+                            <th style="text-align: center">{{ trans('view.admin.work.staff_num') }}</th>
+                            <th style="text-align: center">{{ trans('view.admin.work.today_staff') }}
+                            <th style="text-align: center">{{ trans('view.admin.work.today_order_num') }}
+                            <th style="text-align: center">{{ trans('view.admin.work.profit') }}
+                            <th style="text-align: center">{{ trans('view.admin.work.waiting') }}</th>
+                            <th style="text-align: center">{{ trans('view.admin.work.hint') }}</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($shops as $shop)
+                            <tr>
+                                <td>{{ $shop->name }}</td>
+                                <td>{{ $shop->hasOneManager ? $shop->hasOneManager->display_name:'-' }}</td>
+                                <td>{{ $shop->hasManyUsers->count() }}</td>
+                                <td>{{ $shop->onWork }}</td>
+                                <td>{{ $shop->doneOrders }}</td>
+                                <td>{{ $shop->todayProfit/100 }}</td>
+                                <td>{{ $shop->waitingOrders }}</td>
+                                @if($shop->waitingOrders<3)
                                     <td>
-                                    <button class="btn btn-success btn-sm"
-                                    disabled>{{ trans('view.admin.work.green') }}</button>
+                                        <button class="btn btn-success btn-sm"
+                                                disabled>{{ trans('view.admin.work.green') }}</button>
                                     </td>
-                                    @elseif($shop->hasManyOrderWaiting->count() <= 5)
+                                @elseif($shop->waitingOrders<= 5)
                                     <td>
-                                    <button class="btn btn-warning btn-sm"
-                                    disabled>{{ trans('view.admin.work.yellow') }}</button>
+                                        <button class="btn btn-warning btn-sm"
+                                                disabled>{{ trans('view.admin.work.yellow') }}</button>
                                     </td>
-                                    @else
+                                @else
                                     <td>
-                                    <button class="btn btn-danger btn-sm"
-                                    disabled>{{ trans('view.admin.work.red') }}</button>
+                                        <button class="btn btn-danger btn-sm"
+                                                disabled>{{ trans('view.admin.work.red') }}</button>
                                     </td>
-                                    @endif
-                                </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+                                @endif
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
                     <div class="panel-footer">{{ $shops->links() }}</div>
                 </div>
             </div>
